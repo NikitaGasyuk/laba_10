71b9669f1a0d63d3a6b246fe304c3524e2c79a97
NikitaGasyuk <126286242+NikitaGasyuk@users.noreply.github.com>
2023-11-20 17:09:45 +0300

    Create mat.py

---
+++ mat.py
@@ -0,0 +1,10 @@
+#написать функцию, которая создает массив из всех четных элементов между 50 и 90.
+def create_even_array():
+    even_array = []
+    for num in range(50, 91):
+        if num % 2 == 0:
+            even_array.append(num)
+    return even_array
+
+result = create_even_array()
+print(result)
